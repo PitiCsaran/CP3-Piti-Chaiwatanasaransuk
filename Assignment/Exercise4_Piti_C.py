2ef2cb579bb2e8300e73888cc2801bdda7f1ff48
PitiCsaran <piticsaran@gmail.com>
2021-12-11 08:04:48 +0700

    Initiate Ex4

---
+++ Assignment/Exercise4_Piti_C.py
@@ -0,0 +1,10 @@
+FE_score = 95.0
+GB_score = 80.0
+ICS_score = 75.5
+CP_score = 100.0
+
+print('--- Your Score ---')
+print('Foundation English :', FE_score)
+print('General Business :', GB_score)
+print('Introduction to Computer Systems :', ICS_score)
+print('Computer Programming :', CP_score)
